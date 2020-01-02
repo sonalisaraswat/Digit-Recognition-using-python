c42a9c846c83f9316238916e3f9a8c57c6901259
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2020-01-02 20:51:02 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as pt
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+data=pd.read_csv("datasets\train.csv").as_matrix()
+clf=DecisionTreeClassifier()
+
+#training dataset
+xtrain = data[0:21000,1:]
+train_label=data[0:21000,0]
+
+clf.fit(xtrain,train_label)
+
+#testing data
+xtest=data[21000:,1:1]
+actual_label=data[21000:,0]
+
+d=xtest[8]
+d.shape=(28,28)
+pt.imshow(255-d,cmap='gray')
+print(clf.predict([xtest[8]]))
+pt.show()
+
+p=clf.predict(xtest)
+
+count=0
+for i in range(0,21000):
+    count+=1 if p[i]==actual_label[i] else 0
+print("accuracy=",(count/21000)*100)
